93299edd1ae6765c79ef029772eb60757f4654a8
Haoyu Weng <u7170234@anu.edu.au>
2022-08-23 09:21:40 +0800

    Finish Login Page development

---
+++ fireapp/lib/layout/dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showToast(var text) {
+  Fluttertoast.showToast(
+      msg: text.toString(),
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.,
+      timeInSecForIosWeb: 3,
+      backgroundColor: Colors.black45,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
+
+void showNothing() {}
--- fireapp/lib/main.dart
+++ fireapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'layout/wrapper.dart';
+import 'package:fireapp/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,9 +24,11 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/':(context) => const BasicWrapper(page: LoginPage()),
+      },
     );
   }
 }
