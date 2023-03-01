8297f52ccdb006fefad9408b736acfc1523970c1
Ben McLean <benmclean007@gmail.com>
2023-03-01 23:55:39 +1100

    Add unit test

--- fireapp/lib/domain/repository/authentication_repository.dart
+++ fireapp/lib/domain/repository/authentication_repository.dart
@@ -4,22 +4,35 @@ import 'package:fireapp/data/persistence/authentication_persistence.dart';
 import 'package:fireapp/domain/models/token_response.dart';
 import 'package:injectable/injectable.dart';
 
+/// A repository for handling user authentication.
+///
+/// This class provides a way to log in and retrieve the current session for a user.
 @injectable
 class AuthenticationRepository {
 
   final AuthenticationClient authenticationClient;
   final AuthenticationPersistence authenticationPersistence;
 
-  AuthenticationRepository(this.authenticationClient, this.authenticationPersistence);
+  /// Constructs a new [AuthenticationRepository] with the given dependencies.
+  ///
+  /// The [authenticationClient] is used to perform login requests, and the [authenticationPersistence] is used to
+  /// store and retrieve the current session token.
+  AuthenticationRepository(this.authenticationClient,
+      this.authenticationPersistence);
 
+  /// Logs in a user with the given email and password.
+  ///
+  /// Returns a [TokenResponse] containing the session token.
   Future<TokenResponse> login(String email, String password) async {
     TokenResponse response = await authenticationClient.login(email, password);
     authenticationPersistence.set(response);
     return response;
   }
 
+  /// Retrieves the current session for the user.
+  ///
+  /// Returns a [TokenResponse] containing the session token, or `null` if no session is currently active.
   Future<TokenResponse?> getCurrentSession() async {
     return authenticationPersistence.get();
   }
-
 }
---
+++ fireapp/test/domain/repository/authentication_repository.dart
@@ -0,0 +1,60 @@
+import 'package:fireapp/data/client/authentication_client.dart';
+import 'package:fireapp/data/persistence/authentication_persistence.dart';
+import 'package:fireapp/domain/models/token_response.dart';
+import 'package:fireapp/domain/repository/authentication_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAuthenticationClient extends Mock implements AuthenticationClient {}
+
+class MockAuthenticationPersistence extends Mock implements AuthenticationPersistence {}
+
+void main() {
+  late AuthenticationRepository authenticationRepository;
+  late MockAuthenticationClient authenticationClient;
+  late MockAuthenticationPersistence authenticationPersistence;
+
+  setUp(() {
+    authenticationClient = MockAuthenticationClient();
+    authenticationPersistence = MockAuthenticationPersistence();
+    authenticationRepository = AuthenticationRepository(authenticationClient, authenticationPersistence);
+  });
+
+  group('AuthenticationRepository', () {
+    test('login should return token response', () async {
+      // Arrange
+      const email = 'test@example.com';
+      const password = 'test_password';
+      const tokenResponse = TokenResponse(
+        accessToken: 'test_token',
+        userId: "test_user",
+        role: "role"
+      );
+      when(authenticationClient.login(email, password)).thenAnswer((_) async => tokenResponse);
+
+      // Act
+      final result = await authenticationRepository.login(email, password);
+
+      // Assert
+      expect(result, tokenResponse);
+      verify(authenticationPersistence.set(tokenResponse)).called(1);
+    });
+
+    test('getCurrentSession should return token response', () async {
+      // Arrange
+      const tokenResponse = TokenResponse(
+        accessToken: 'test_token',
+        userId: "test_user",
+        role: "role"
+      );
+      when(authenticationPersistence.get()).thenAnswer((_) async => tokenResponse);
+
+      // Act
+      final result = await authenticationRepository.getCurrentSession();
+
+      // Assert
+      expect(result, tokenResponse);
+      verify(authenticationPersistence.get()).called(1);
+    });
+  });
+}
