e1d4d1197c9b151472a808ab1d59297d39bb39af
Ben McLean <benmclean007@gmail.com>
2023-04-16 19:19:22 +1000

    Add test for persistence

---
+++ fireapp/test/data/persistence/authentication_persistence_test.dart
@@ -0,0 +1,31 @@
+import 'package:fireapp/data/persistence/authentication_persistence.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fireapp/domain/models/token_response.dart';
+
+void main() {
+  group('AuthenticationPersistence', () {
+    late AuthenticationPersistence authenticationPersistence;
+
+    setUp(() {
+      authenticationPersistence = AuthenticationPersistence();
+    });
+
+    test('token is null initially', () {
+      expect(authenticationPersistence.token, isNull);
+    });
+
+    test('set() and get() work correctly', () async {
+      const tokenResponse = TokenResponse(
+        userId: "",
+        accessToken: 'test_token',
+        role: ""
+      );
+      await authenticationPersistence.set(tokenResponse);
+
+      final cachedToken = await authenticationPersistence.get();
+
+      expect(cachedToken, equals(tokenResponse));
+      expect(authenticationPersistence.token, equals('test_token'));
+    });
+  });
+}
