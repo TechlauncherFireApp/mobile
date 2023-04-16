49a15a9b06b4e47b11a359b17575481c1c95d86b
Ben McLean <benmclean007@gmail.com>
2023-04-17 00:43:02 +1000

    Wrote tests for volunteer list viewmodel

---
+++ fireapp/test/request_state_has_content_matcher.dart
@@ -0,0 +1,31 @@
+
+import 'package:fireapp/domain/request_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class RequestStateHasContentMatcher<T> extends CustomMatcher {
+
+  RequestStateHasContentMatcher(matcher) : super("RequestState result is", "result", matcher);
+  @override
+  featureValueOf(actual) {
+    if (actual is SuccessRequestState<T>) {
+      return actual.result;
+    } else {
+      return null;
+    }
+  }
+
+}
+
+class RequestStateHasExceptionMatcher<T> extends CustomMatcher {
+
+  RequestStateHasExceptionMatcher(matcher) : super("RequestState exception is", "exception", matcher);
+  @override
+  featureValueOf(actual) {
+    if (actual is ExceptionRequestState<T>) {
+      return actual.exception;
+    } else {
+      return null;
+    }
+  }
+
+}
