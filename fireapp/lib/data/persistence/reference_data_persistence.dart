be707f85d066e7db836a650b8e0d77fca5434e1b
Ben McLean <benmclean007@gmail.com>
2023-04-17 18:43:36 +1000

    Fix dependency injection of reference data persistence

--- fireapp/lib/data/persistence/reference_data_persistence.dart
+++ fireapp/lib/data/persistence/reference_data_persistence.dart
@@ -1,6 +1,7 @@
 
 import 'package:fireapp/domain/models/reference/reference_data.dart';
 import 'package:fireapp/domain/models/reference/reference_data_db.dart';
+import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 import 'db/reference_data_db_dao.dart';
@@ -9,17 +10,31 @@ import 'db/reference_data_db_metadata_dao.dart';
 @injectable
 class ReferenceDataPersistence {
 
-  final ReferenceDataDbDao _dataDbDao;
-  final ReferenceDataDbMetadataDao _dataDbMetadataDao;
-  ReferenceDataPersistence(this._dataDbDao, this._dataDbMetadataDao);
+  late ReferenceDataDbDao _dataDbDao;
+  late ReferenceDataDbMetadataDao _dataDbMetadataDao;
+  late Future<void> isSetup;
+
+  // This should be done through DI (hence .di), but asynchronous constructors are wack
+  @factoryMethod
+  ReferenceDataPersistence() {
+    isSetup = () async {
+      _dataDbDao = await GetIt.instance.getAsync();
+      _dataDbMetadataDao = await GetIt.instance.getAsync();
+    }();
+  }
+  ReferenceDataPersistence.di(this._dataDbDao, this._dataDbMetadataDao) {
+    isSetup = Future.value(null);
+  }
 
   Future<DateTime?> getLastUpdated(ReferenceDataType type) async {
+    await isSetup;
     var metadata = await _dataDbMetadataDao.getByType(type.name);
     if (metadata == null) return null;
     return DateTime.fromMillisecondsSinceEpoch(metadata.lastRefreshed);
   }
 
   Future<void> save(ReferenceDataType type, List<ReferenceDataDb> data) async {
+    await isSetup;
     _dataDbMetadataDao.insertReferenceData(ReferenceDataDbMetadata(
       type: type.name,
       lastRefreshed: DateTime.now().millisecondsSinceEpoch
@@ -28,6 +43,7 @@ class ReferenceDataPersistence {
   }
 
   Future<List<ReferenceDataDb>> getReferenceData(ReferenceDataType type) async {
+    await isSetup;
     return _dataDbDao.getByType(type.name);
   }
 
