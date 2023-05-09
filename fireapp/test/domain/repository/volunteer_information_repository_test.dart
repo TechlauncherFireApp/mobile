16820dd8c4fa8bca27e99a9f6fdf3dc7bc622146
EzekielNicholas123 <u7510973@anu.edu.au>
2023-05-09 16:21:59 +1000

    Fix Some Testing for the repository

--- fireapp/test/domain/repository/volunteer_information_repository_test.dart
+++ fireapp/test/domain/repository/volunteer_information_repository_test.dart
@@ -1,96 +1,125 @@
-// import 'package:fireapp/domain/repository/authentication_repository.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-// import 'package:mockito/annotations.dart';
-// import 'package:fireapp/data/client/volunteer_information_client.dart';
-// import 'package:fireapp/domain/models/dto/volunteer_information_dto.dart';
-// import 'package:fireapp/domain/models/reference/qualification.dart';
-// import 'package:fireapp/domain/models/volunteer_information.dart';
-// import 'package:fireapp/domain/repository/reference_data_repository.dart';
-// import 'package:fireapp/domain/repository/volunteer_information_repository.dart';
-//
-// @GenerateNiceMocks([
-//   MockSpec<VolunteerInformationClient>(),
-//   MockSpec<ReferenceDataRepository>(),
-// ])
-// import 'volunteer_information_repository_test.mocks.dart';
-//
-// void main() {
-//   group('VolunteerInformationRepository', () {
-//     late VolunteerInformationRepository volunteerInformationRepository;
-//     late MockVolunteerInformationClient mockClient;
-//     late MockReferenceDataRepository mockReferenceDataRepository;
-//     late AuthenticationRepository mockAuthenticationRepository;
-//
-//     setUp(() {
-//       mockClient = MockVolunteerInformationClient();
-//       mockReferenceDataRepository = MockReferenceDataRepository();
-//       volunteerInformationRepository = VolunteerInformationRepository(mockClient, mockReferenceDataRepository, mockAuthenticationRepository);
-//     });
-//
-//     test('getVolunteerInformation() returns a VolunteerInformation object', () async {
-//       const expectedVolunteerInformationDto = VolunteerInformationDto(
-//         ID: '1',
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         email: 'johndoe@example.com',
-//         mobileNo: '1234567890',
-//         prefHours: 10,
-//         expYears: 2,
-//         qualifications: ['1', '2'],
-//         availabilities: AvailabilityFieldDto(
-//           monday: [[1,8]],
-//           tuesday: [[]],
-//           wednesday: [[]],
-//           thursday: [[]],
-//           friday: [[]],
-//           saturday: [[]],
-//           sunday: [[]],
-//         ),
-//         possibleRoles: ['Role 1', 'Role 2'],
-//       );
-//       final expectedQualifications = [
-//         Qualification(
-//           id: 1,
-//           name: 'Qualification 1',
-//           updated: DateTime.now(),
-//           created: DateTime.now(),
-//         ),
-//         Qualification(
-//           id: 2,
-//           name: 'Qualification 2',
-//           updated: DateTime.now(),
-//           created: DateTime.now(),
-//         )
-//       ];
-//
-//       final expectedVolunteerInformation = VolunteerInformation(
-//         ID: '1',
-//         firstName: 'John',
-//         lastName: 'Doe',
-//         email: 'johndoe@example.com',
-//         mobileNo: '1234567890',
-//         prefHours: 10,
-//         expYears: 2,
-//         qualifications: expectedQualifications,
-//         availabilities: const AvailabilityField(
-//           monday: [[1,8]],
-//           tuesday: [[]],
-//           wednesday: [[]],
-//           thursday: [[]],
-//           friday: [[]],
-//           saturday: [[]],
-//           sunday: [[]],
-//         ),
-//         possibleRoles: ['Role 1', 'Role 2'],
-//       );
-//
-//       when(mockClient.getVolunteerInformation('1')).thenAnswer((_) => Future.value(expectedVolunteerInformationDto));
-//       when(mockReferenceDataRepository.getQualifications()).thenAnswer((_) => Future.value(expectedQualifications));
-//
-//       final volunteerInformation = await volunteerInformationRepository.getVolunteerInformation('1');
-//
-//       expect(volunteerInformation, equals(expectedVolunteerInformation));
-//     });
-//   });
-// }
+import 'package:fireapp/domain/models/reference/volunteer_role.dart';
+import 'package:fireapp/domain/models/token_response.dart';
+import 'package:fireapp/domain/repository/authentication_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:fireapp/data/client/volunteer_information_client.dart';
+import 'package:fireapp/domain/models/dto/volunteer_information_dto.dart';
+import 'package:fireapp/domain/models/reference/qualification.dart';
+import 'package:fireapp/domain/models/volunteer_information.dart';
+import 'package:fireapp/domain/repository/reference_data_repository.dart';
+import 'package:fireapp/domain/repository/volunteer_information_repository.dart';
+
+@GenerateNiceMocks([
+  MockSpec<VolunteerInformationClient>(),
+  MockSpec<ReferenceDataRepository>(),
+  MockSpec<AuthenticationRepository>(),
+])
+import 'volunteer_information_repository_test.mocks.dart';
+
+void main() {
+  group('VolunteerInformationRepository', () {
+    late VolunteerInformationRepository volunteerInformationRepository;
+    late MockVolunteerInformationClient mockClient;
+    late MockReferenceDataRepository mockReferenceDataRepository;
+    late MockAuthenticationRepository mockAuthenticationRepository;
+
+    setUp(() {
+      mockClient = MockVolunteerInformationClient();
+      mockReferenceDataRepository = MockReferenceDataRepository();
+      mockAuthenticationRepository = MockAuthenticationRepository();
+      volunteerInformationRepository = VolunteerInformationRepository(mockClient, mockReferenceDataRepository, mockAuthenticationRepository);
+    });
+
+    test('getVolunteerInformation() returns a VolunteerInformation object', () async {
+      const expectedVolunteerInformationDto = VolunteerInformationDto(
+        ID: '1',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        mobileNo: '1234567890',
+        prefHours: 10,
+        expYears: 2,
+        qualifications: ['1', '2'],
+        availabilities: AvailabilityFieldDto(
+          monday: [[1,8]],
+          tuesday: [[]],
+          wednesday: [[]],
+          thursday: [[]],
+          friday: [[]],
+          saturday: [[]],
+          sunday: [[]],
+        ),
+        possibleRoles: ['Role 1', 'Role 2'],
+      );
+      final expectedQualifications = [
+        Qualification(
+          id: 1,
+          name: 'Qualification 1',
+          updated: DateTime.now(),
+          created: DateTime.now(),
+        ),
+        Qualification(
+          id: 2,
+          name: 'Qualification 2',
+          updated: DateTime.now(),
+          created: DateTime.now(),
+        )
+      ];
+
+      final expectedVolunteerInformation = VolunteerInformation(
+        ID: '1',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        mobileNo: '1234567890',
+        prefHours: 10,
+        expYears: 2,
+        qualifications: expectedQualifications,
+        availabilities: const AvailabilityField(
+          monday: [[1,8]],
+          tuesday: [[]],
+          wednesday: [[]],
+          thursday: [[]],
+          friday: [[]],
+          saturday: [[]],
+          sunday: [[]],
+        ),
+        possibleRoles: ['Role 1', 'Role 2'],
+      );
+
+      when(mockClient.getVolunteerInformation('1')).thenAnswer((_) => Future.value(expectedVolunteerInformationDto));
+      when(mockReferenceDataRepository.getQualifications()).thenAnswer((_) => Future.value(expectedQualifications));
+
+      final volunteerInformation = await volunteerInformationRepository.getVolunteerInformation('1');
+
+      expect(volunteerInformation, equals(expectedVolunteerInformation));
+    });
+    test('successfully updates roles', () async {
+      // Arrange
+      var roles = [VolunteerRole(id: 1, name: 'Role 1', updated: DateTime.now(), created: DateTime.now())];
+      var allRoles = [VolunteerRole(id: 1, name: 'Role 1', updated: DateTime.now(), created: DateTime.now()),
+        VolunteerRole(id: 2, name: 'Role 2', updated: DateTime.now(), created: DateTime.now())];
+      var userId = 12345;
+
+      when(mockReferenceDataRepository.getRoles()).thenAnswer((_) async => allRoles);
+      when(mockAuthenticationRepository.getCurrentSession()).thenAnswer((_) async => TokenResponse(userId: userId, accessToken: 'token', role: 'role'));
+
+      // Act
+      await volunteerInformationRepository.updateRoles(roles);
+
+      // Assert
+      verify(mockClient.updateRoles(userId, roles, [allRoles[1]])).called(1);
+    });
+
+    test('throws an exception when user ID is null', () async {
+      // Arrange
+      var roles = [VolunteerRole(id: 1, name: 'Role 1', updated: DateTime.now(), created: DateTime.now())];
+      when(mockAuthenticationRepository.getCurrentSession()).thenAnswer((_) async => null);
+
+      // Act & Assert
+      expect(() => volunteerInformationRepository.updateRoles(roles), throwsA(isA<Exception>()));
+    });
+  });
+}
--- fireapp/test/presentation/change_roles/change_roles_view_model_test.dart
+++ fireapp/test/presentation/change_roles/change_roles_view_model_test.dart
@@ -1,4 +1,7 @@
+import 'package:fireapp/domain/models/reference/qualification.dart';
 import 'package:fireapp/domain/models/reference/volunteer_role.dart';
+import 'package:fireapp/domain/models/volunteer_information.dart';
+import 'package:fireapp/domain/models/volunteer_listing.dart';
 import 'package:fireapp/domain/repository/reference_data_repository.dart';
 import 'package:fireapp/domain/repository/volunteer_information_repository.dart';
 import 'package:fireapp/domain/request_state.dart';
@@ -9,7 +12,6 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rxdart/rxdart.dart';
 
-import '../volunteer_information/volunteer_information_viewmodel_test.mocks.dart';
 import 'change_roles_view_model_test.mocks.dart';
 
 @GenerateNiceMocks([
@@ -18,109 +20,154 @@ import 'change_roles_view_model_test.mocks.dart';
 ])
 
 void main() {
-  late ChangeRolesViewModel viewModel;
-  late MockReferenceDataRepository referenceDataRepository;
-  late MockVolunteerInformationRepository volunteerInformationRepository;
+  const volunteers = <VolunteerListing>[
+    VolunteerListing(volunteerId: "1", name: 'John Doe'),
+    VolunteerListing(volunteerId: "2", name: 'Jane Doe'),
+  ];
 
-  setUp(() {
-    referenceDataRepository = MockReferenceDataRepository();
-    volunteerInformationRepository = MockVolunteerInformationRepository();
-    viewModel = ChangeRolesViewModel(
-        referenceDataRepository, volunteerInformationRepository);
-  });
+  group('ChangeRolesViewModel', ()
+  {
+    late MockReferenceDataRepository referenceDataRepository;
+    late MockVolunteerInformationRepository volunteerInformationRepository;
+    late ChangeRolesViewModel viewModel;
 
-  test('loads user roles successfully', () async {
-    final roles = [
-      VolunteerRole(id: 1,
-          name: 'Role 1',
-          created: DateTime.now(),
-          updated: DateTime.now()),
-      VolunteerRole(id: 2,
-          name: 'Role 2',
-          created: DateTime.now(),
-          updated: DateTime.now()),
+    final expectedQualifications = [
+      Qualification(
+        id: 1,
+        name: 'Qualification 1',
+        updated: DateTime.now(),
+        created: DateTime.now(),
+      ),
+      Qualification(
+        id: 2,
+        name: 'Qualification 2',
+        updated: DateTime.now(),
+        created: DateTime.now(),
+      ),
     ];
 
-    when(referenceDataRepository.getRoles()).thenAnswer((_) async => roles);
+    VolunteerInformation information = VolunteerInformation(
+      ID: '1',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@example.com',
+      mobileNo: '1234567890',
+      prefHours: 10,
+      expYears: 2,
+      qualifications: expectedQualifications,
+      availabilities: const AvailabilityField(
+        monday: [[1, 8]],
+        tuesday: [[]],
+        wednesday: [[]],
+        thursday: [[]],
+        friday: [[]],
+        saturday: [[]],
+        sunday: [[]],
+      ),
+      possibleRoles: ['Role 1', 'Role 2'],
+    );
 
-    viewModel.init('volunteerId', ['2']);
-    await Future.delayed(
-        Duration(milliseconds: 100)); // Give time for async code to execute
+    setUp(() {
+      referenceDataRepository = MockReferenceDataRepository();
+      volunteerInformationRepository = MockVolunteerInformationRepository();
+      viewModel = ChangeRolesViewModel(
+          referenceDataRepository, volunteerInformationRepository);
+    });
 
-    final userRolesState = viewModel.userRoles as BehaviorSubject<
-        RequestState<List<UserRole>>>;
-    expect(userRolesState.value, isA<SuccessRequestState<List<UserRole>>>());
-    expect((userRolesState.value as SuccessRequestState).result, isNotEmpty);
-  });
+    test('loads user roles successfully', () async {
+      final roles = [
+        VolunteerRole(id: 1,
+            name: 'Role 1',
+            created: DateTime.now(),
+            updated: DateTime.now()),
+        VolunteerRole(id: 2,
+            name: 'Role 2',
+            created: DateTime.now(),
+            updated: DateTime.now()),
+      ];
 
-  test('updates a user role', () async {
-    final roles = [
-      VolunteerRole(id: 1,
-          name: 'Role 1',
-          created: DateTime.now(),
-          updated: DateTime.now()),
-      VolunteerRole(id: 2,
-          name: 'Role 2',
-          created: DateTime.now(),
-          updated: DateTime.now()),
-    ];
+      when(referenceDataRepository.getRoles()).thenAnswer((_) async => roles);
 
-    when(referenceDataRepository.getRoles()).thenAnswer((_) async => roles);
+      viewModel.init('volunteerId', ['2']);
+      await Future.delayed(
+          Duration(milliseconds: 100)); // Give time for async code to execute
 
-    viewModel.init('volunteerId', ['2']);
-    await Future.delayed(
-        Duration(milliseconds: 100)); // Give time for async code to execute
+      final userRolesState = viewModel.userRoles as BehaviorSubject<
+          RequestState<List<UserRole>>>;
+      expect(userRolesState.value, isA<SuccessRequestState<List<UserRole>>>());
+      expect((userRolesState.value as SuccessRequestState).result, isNotEmpty);
+    });
 
-    final userRolesState = viewModel.userRoles as BehaviorSubject<
-        RequestState<List<UserRole>>>;
-    final userRole = (userRolesState.value as SuccessRequestState<
-        List<UserRole>>).result[0];
+    test('updates a user role', () async {
+      final roles = [
+        VolunteerRole(id: 1,
+            name: 'Role 1',
+            created: DateTime.now(),
+            updated: DateTime.now()),
+        VolunteerRole(id: 2,
+            name: 'Role 2',
+            created: DateTime.now(),
+            updated: DateTime.now()),
+      ];
 
-    viewModel.updateRole(userRole);
-    await Future.delayed(
-        Duration(milliseconds: 100)); // Give time for async code to execute
+      when(referenceDataRepository.getRoles()).thenAnswer((_) async => roles);
 
-    expect(userRolesState.value, isA<SuccessRequestState<List<UserRole>>>());
-    expect((userRolesState.value as SuccessRequestState).result[0].checked,
-        !userRole.checked);
-  });
+      viewModel.init('volunteerId', ['2']);
+      await Future.delayed(
+          Duration(milliseconds: 100)); // Give time for async code to execute
 
-  test('submits user roles successfully', () async {
-    final roles = [
-      VolunteerRole(id: 1,
-          name: 'Role 1',
-          created: DateTime.now(),
-          updated: DateTime.now()),
-      VolunteerRole(id: 2,
-          name: 'Role 2',
-          created: DateTime.now(),
-          updated: DateTime.now()),
-    ];
+      final userRolesState = viewModel.userRoles as BehaviorSubject<
+          RequestState<List<UserRole>>>;
+      final userRole = (userRolesState.value as SuccessRequestState<
+          List<UserRole>>).result[0];
 
-    when(referenceDataRepository.getRoles()).thenAnswer((_) async => roles);
+      viewModel.updateRole(userRole);
+      await Future.delayed(
+          Duration(milliseconds: 100)); // Give time for async code to execute
 
-    viewModel.init('volunteerId', ['2']);
-    await Future.delayed(
-        Duration(milliseconds: 100)); // Give time for async code to execute
+      expect(userRolesState.value, isA<SuccessRequestState<List<UserRole>>>());
+      expect((userRolesState.value as SuccessRequestState).result[0].checked,
+          !userRole.checked);
+    });
 
-    viewModel.submit();
-    await Future.delayed(
-        Duration(milliseconds: 100)); // Give time for async code to execute
+    test('submits user roles successfully', () async {
+      final roles = [
+        VolunteerRole(id: 1,
+            name: 'Role 1',
+            created: DateTime.now(),
+            updated: DateTime.now()),
+        VolunteerRole(id: 2,
+            name: 'Role 2',
+            created: DateTime.now(),
+            updated: DateTime.now()),
+      ];
 
-    final userRolesState = viewModel.userRoles as BehaviorSubject<
-        RequestState<List<UserRole>>>;
-    final selectedRoles = (userRolesState.value as SuccessRequestState<
-        List<UserRole>>)
-        .result
-        .where((r) => r.checked)
-        .map((r) => r.role)
-        .toList();
+      when(referenceDataRepository.getRoles()).thenAnswer((_) async => roles);
 
-    verify(volunteerInformationRepository.updateRoles(selectedRoles)).called(1);
+      viewModel.init('volunteerId', ['2']);
+      await Future.delayed(
+          Duration(milliseconds: 100)); // Give time for async code to execute
 
-    final submissionState = viewModel.submissionState as BehaviorSubject<
-        RequestState<void>>;
-    expect(submissionState.value, isA<SuccessRequestState<void>>());
+      viewModel.submit();
+      await Future.delayed(
+          Duration(milliseconds: 100)); // Give time for async code to execute
+
+      final userRolesState = viewModel.userRoles as BehaviorSubject<
+          RequestState<List<UserRole>>>;
+      final selectedRoles = (userRolesState.value as SuccessRequestState<
+          List<UserRole>>)
+          .result
+          .where((r) => r.checked)
+          .map((r) => r.role)
+          .toList();
+
+      verify(volunteerInformationRepository.updateRoles(selectedRoles)).called(
+          1);
+
+      final submissionState = viewModel.submissionState as BehaviorSubject<
+          RequestState<void>>;
+      expect(submissionState.value, isA<SuccessRequestState<void>>());
+    });
   });
 }
 
