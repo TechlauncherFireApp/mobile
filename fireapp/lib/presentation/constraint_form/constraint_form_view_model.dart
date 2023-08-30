74f45640bc76a490930956a2b81f909cc42d9568
Nay Chi Than Shwe <u7561626@anu.edu.au>
2023-08-30 14:30:13 +1000

    submitForm fixed

--- fireapp/lib/presentation/constraint_form/constraint_form_view_model.dart
+++ fireapp/lib/presentation/constraint_form/constraint_form_view_model.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../../domain/request_state.dart';
 
 class ConstraintFormViewModel {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -9,6 +12,10 @@ class ConstraintFormViewModel {
   final TextEditingController inputDateController = TextEditingController();
   final TextEditingController startTimeController = TextEditingController();
 
+  // RequestState stream
+  final BehaviorSubject<RequestState<void>> _state = BehaviorSubject.seeded(RequestState.initial());
+  Stream<RequestState<void>> get state => _state.stream;
+
   // Dummy values for the assets list
   final Stream<List<int>> assetsStream = Stream<List<int>>.value([1, 2, 3, 4]);
 
@@ -17,13 +24,18 @@ class ConstraintFormViewModel {
 
   // Function to be called when the form is submitted
   void submitForm() {
-    formKey.currentState!.validate();
-      // sending the data to database or other page
-}
+    if (formKey.currentState?.validate() == true) {
+      // add submission logic here
+    } else {
+      // If the form is not valid, handle accordingly
+    }
+  }
 
-  void dispose() {
+
+  void dispose() { // Dispose of resources
     titleController.dispose();
     inputDateController.dispose();
     startTimeController.dispose();
+    _state.close();
   }
 }
