4dfe743058b19c0a11fe084bc711d5cf18510feb
Nay Chi Than Shwe <u7561626@anu.edu.au>
2023-09-20 09:01:23 +1000

    , added in main.dart

--- fireapp/lib/main.dart
+++ fireapp/lib/main.dart
@@ -64,7 +64,7 @@ class _MyAppState extends State<MyApp> {
         '/volunteer_list': (context) => const VolunteerList(),
         '/dietary_requirements/update': (context) => const DietaryRequirementsPage(),
         '/server_url_page': (context) => const ServerUrlPage(),
-        '/shift_request': (context) => const ShiftRequestView(requestId: "1")
+        '/shift_request': (context) => const ShiftRequestView(requestId: "1"),
         '/constraint_form': (context) => const SchedulerConstraintPage(),
       },
     );
