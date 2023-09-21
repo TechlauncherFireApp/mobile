6c3e82ece1c5a07883e08c18d113ded0399c0521
Nay Chi Than Shwe <u7561626@anu.edu.au>
2023-09-21 21:51:26 +1000

    scheduler_constraint_form_repository.dart link to constraint_form_view_model.dart

--- fireapp/lib/presentation/constraint_form/constraint_form_view_model.dart
+++ fireapp/lib/presentation/constraint_form/constraint_form_view_model.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
+import 'package:fireapp/domain/repository/scheduler_constraint_form_repository.dart';
 import 'package:fireapp/presentation/fireapp_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
 import '../../domain/models/reference/asset_type.dart';
+import '../../domain/request_state.dart';
+import '../../global/di.dart';
 
 class SchedulerConstraintFormViewModel extends FireAppViewModel {
   late final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  late final SchedulerConstraintFormRepository
+      _schedulerConstraintFormRepository;
 
   // Text Editing Controllers
   final TextEditingController titleController = TextEditingController();
@@ -16,21 +21,25 @@ class SchedulerConstraintFormViewModel extends FireAppViewModel {
   // Remove this after actual thing implemented
   // coverage:ignore-start
 
-
-  final BehaviorSubject<List<AssetType>> _assetTypes = BehaviorSubject<List<AssetType>>();
+  final BehaviorSubject<List<AssetType>> _assetTypes =
+      BehaviorSubject<List<AssetType>>();
   Stream<List<AssetType>> get assetsStream => _assetTypes.stream;
 
   // Current dropdown value
   int dropdownValue = 1;
 
   // Function to be called when the form is submitted
-  void submitForm() {
-    if (formKey.currentState?.validate() != true) return;
-    // Submission logic here
+  void submit(int id, String name, String code, DateTime updated,
+      DateTime created) async {
+    try {
+      await _schedulerConstraintFormRepository.getAssetType(
+          id, name, code, updated, created);
+      _assetTypes.add(RequestState.success(null) as List<AssetType>);
+    } catch (e) {
+      logger.e(e);
+      _assetTypes.add(RequestState.exception(e) as List<AssetType>);
+    }
   }
-
-  // coverage:ignore-ends
-
 
   @override
   Future<void> dispose() async {
