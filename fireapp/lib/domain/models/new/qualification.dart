3938a986c9cb897ba9311a3a75674236011c363e
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2024-03-02 11:38:37 +1100

    re-add the model of qualification

---
+++ fireapp/lib/domain/models/new/qualification.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'qualification.freezed.dart';
+part 'qualification.g.dart';
+
+@freezed
+class Qualification with _$Qualification {
+  const factory Qualification({
+    required int id,
+    required String name,
+    required bool deleted,
+    @JsonKey(name: 'last_update_datetime') DateTime? updateDateTime,
+    @JsonKey(name: 'created_datetime') DateTime? insertDateTime,
+  }) = _Qualification;
+
+  factory Qualification.fromJson(Map<String, dynamic> json) => _$QualificationFromJson(json);
+}
