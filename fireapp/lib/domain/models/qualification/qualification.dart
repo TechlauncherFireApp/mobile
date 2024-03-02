a2181a4ee4bef0a9933f8fad6e6f2f3f5dc391bd
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2024-03-03 00:47:16 +1100

    Fix the name

--- fireapp/lib/domain/models/qualification/qualification.dart
+++ fireapp/lib/domain/models/qualification/qualification.dart
@@ -10,8 +10,8 @@ class Qualification with _$Qualification {
     required int id,
     required String name,
     required bool deleted,
-    @JsonKey(name: 'update_datetime') DateTime? updateDateTime,
-    @JsonKey(name: 'insert_datetime') DateTime? insertDateTime,
+    @JsonKey(name: 'update_date_time') DateTime? updateDateTime,
+    @JsonKey(name: 'insert_date_time') DateTime? insertDateTime,
   }) = _Qualification;
 
   factory Qualification.fromJson(Map<String, dynamic> json) => _$QualificationFromJson(json);
