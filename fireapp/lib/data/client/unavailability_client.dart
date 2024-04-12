196acbbcaa6755a4f74581ddc3df22f1c00e3bf1
Ethan Teber-Rossi <ethantrossi@gmail.com>
2024-04-12 19:44:05 +1000

    Update methods in unavailability_repository and unavailability_client.dart to include edit, get and delete.

--- fireapp/lib/data/client/unavailability_client.dart
+++ fireapp/lib/data/client/unavailability_client.dart
@@ -1,5 +1,6 @@
 import 'package:fireapp/data/client/api/rest_client.dart';
 import 'package:fireapp/domain/models/unavailability/unavailability_event_post.dart';
+import 'package:fireapp/domain/models/unavailability/unavailability_time.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
@@ -11,5 +12,21 @@ class UnavailabilityClient {
   Future<void> createUnavailabilityEvent(
       int userId, UnavailabilityEventPost newEvent) {
     return restClient.createUnavailabilityEvent(userId, newEvent);
+  }
+
+  //TODO get list of all unavailability events from user
+  void getUnavailabilityEvents(int userId) {
+    return;
+  }
+
+  //TODO edit a user's specific unavailability event
+  void editUnavailabilityEvent(
+      int userId, int eventID, UnavailabilityTime event) {
+    return;
+  }
+
+  //TODO delete a user's specific unavailability event
+  void deleteUnavailabilityEvent(int userId, int eventID) {
+    return;
   }
 }
--- fireapp/lib/domain/repository/unavailability_repository.dart
+++ fireapp/lib/domain/repository/unavailability_repository.dart
@@ -2,6 +2,8 @@ import 'package:fireapp/data/client/unavailability_client.dart';
 import 'package:fireapp/domain/models/unavailability/unavailability_event_post.dart';
 import 'package:injectable/injectable.dart';
 
+import '../models/unavailability/unavailability_time.dart';
+
 @injectable
 class UnavailabilityRepository {
   final UnavailabilityClient unavailabilityClient;
@@ -13,5 +15,21 @@ class UnavailabilityRepository {
   Future<void> createUnavailabilityEvent(
       int userId, UnavailabilityEventPost newEvent) {
     return unavailabilityClient.createUnavailabilityEvent(userId, newEvent);
+  }
+
+  //TODO get list of all unavailability events from user
+  void getUnavailabilityEvents(int userId) {
+    return;
+  }
+
+  //TODO edit a user's specific unavailability event
+  void editUnavailabilityEvent(
+      int userId, int eventID, UnavailabilityTime event) {
+    return;
+  }
+
+  //TODO delete a user's specific unavailability event
+  void deleteUnavailabilityEvent(int userId, int eventID) {
+    return;
   }
 }
