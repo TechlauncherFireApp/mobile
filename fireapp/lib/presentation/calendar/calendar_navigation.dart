06672b792123058a8e7431d06fa5da2f36bad09b
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2024-04-23 17:05:50 +1000

    Revert "Add the eventId in navigation"
    This reverts commit ccd00bb1aae59e141ccfffbeed4cdadd4e6b0bb8.

--- fireapp/lib/presentation/calendar/calendar_navigation.dart
+++ fireapp/lib/presentation/calendar/calendar_navigation.dart
@@ -3,5 +3,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'calendar_navigation.freezed.dart';
 @freezed
 class CalendarNavigation with _$CalendarNavigation {
-  const factory CalendarNavigation.eventDetail(int eventId) = EditEvent;
+  //TODO navigation
+  const factory CalendarNavigation.add() = UnavailabilityForm;
 }
