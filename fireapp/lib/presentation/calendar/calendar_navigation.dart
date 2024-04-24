5314df1bbcbfb60f386c1d3f42b75ebd9e0d61b4
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2024-04-24 15:18:34 +1000

    revert the view part and just do the navigation

--- fireapp/lib/presentation/calendar/calendar_navigation.dart
+++ fireapp/lib/presentation/calendar/calendar_navigation.dart
@@ -1,7 +1,8 @@
+import 'package:fireapp/domain/models/unavailability/unavailability_time.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'calendar_navigation.freezed.dart';
 @freezed
 class CalendarNavigation with _$CalendarNavigation {
-  const factory CalendarNavigation.eventDetail(int eventId) = EditEvent;
+  const factory CalendarNavigation.eventDetail(UnavailabilityTime event) = EditEvent;
 }
