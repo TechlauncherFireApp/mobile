d608401374392b4d0e0d4d8d9939fe5cc43844b2
Ethan Teber-Rossi <ethantrossi@gmail.com>
2024-04-24 13:23:34 +1000

    Start on calendar view model

--- fireapp/lib/presentation/calendar/calendar_view_model.dart
+++ fireapp/lib/presentation/calendar/calendar_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:fireapp/domain/models/unavailability/unavailability_time.dart';
 import 'package:fireapp/domain/repository/authentication_repository.dart';
 import 'package:fireapp/presentation/fireapp_view_model.dart';
 import 'package:injectable/injectable.dart';
@@ -7,6 +8,7 @@ import 'package:rxdart/rxdart.dart';
 import '../../domain/request_state.dart';
 import 'package:fireapp/domain/repository/unavailability_repository.dart';
 
+import '../../global/di.dart';
 import 'calendar_navigation.dart';
 
 @injectable
@@ -16,19 +18,25 @@ class CalendarViewModel extends FireAppViewModel
   late final AuthenticationRepository _authenticationRepository;
   late final UnavailabilityRepository _unavailabilityRepository;
 
-  final BehaviorSubject<int> _selectedMonth = BehaviorSubject();
+  final BehaviorSubject<int> _selectedMonth =
+      BehaviorSubject.seeded(DateTime.now().month);
 
-  final BehaviorSubject<int> _selectedYear = BehaviorSubject();
+  final BehaviorSubject<int> _selectedYear =
+      BehaviorSubject.seeded(DateTime.now().year);
 
+  // List of Volunteer's unavailability events
+  final BehaviorSubject<RequestState<List<UnavailabilityTime>>>
+      _unavailabilityEvents = BehaviorSubject.seeded(RequestState.initial());
+  Stream<RequestState<List<UnavailabilityTime>>> get eventsStream =>
+      _unavailabilityEvents.stream;
 
   // Loading State controllers
-  final BehaviorSubject<RequestState<void>> _submissionState =
-  BehaviorSubject.seeded(RequestState.success(null));
-  Stream<RequestState<void>> get submissionState => _submissionState.stream;
+  final BehaviorSubject<RequestState<void>> _loadingState =
+      BehaviorSubject.seeded(RequestState.success(null));
+  Stream<RequestState<void>> get loadingState => _loadingState.stream;
 
   // Navigation handling
-  final BehaviorSubject<CalendarNavigation> _navigate =
-  BehaviorSubject();
+  final BehaviorSubject<CalendarNavigation> _navigate = BehaviorSubject();
 
   @override
   Stream<CalendarNavigation> get navigate => _navigate.stream;
@@ -37,18 +45,73 @@ class CalendarViewModel extends FireAppViewModel
       this._authenticationRepository, this._unavailabilityRepository);
 
 
-  void displayUnavailabilityEvents(){
+  void fetchUnavailabilityEvents() {
+    _loadingState.add(RequestState.loading());
+    () async {
+      try {
+        var userID =
+            (await _authenticationRepository.getCurrentSession())?.userId;
 
+        // Check if userId is null and throw an exception if it is
+        if (userID == null) {
+          throw Exception(
+              'User ID is null. Cannot update roles without a valid user ID.');
+        }
+        var events; //=
+        _unavailabilityRepository.getUnavailabilityEvents(userID);
+        if (events.isEmpty) {
+          events = [];
+        }
+        _unavailabilityEvents.add(RequestState.success(events));
+      } catch (e) {
+        // Handle errors here
+        _unavailabilityEvents.add(RequestState.exception(e));
+      }
+    };
   }
 
-  void deleteUnavailability(int eventID){
-
+  void filterEvents(){
+    // Go through each UnavalabilityTime
+    // and check if the selected month, selected year is part of start date and end date.
   }
 
-  void updateSelectedMonth(int month){
+  void deleteUnavailability(int eventID) {
+    _loadingState.add(RequestState.loading());
+    () async {
+      try {
+        var userID =
+            (await _authenticationRepository.getCurrentSession())?.userId;
+
+        // Check if userId is null and throw an exception if it is
+        if (userID == null) {
+          throw Exception(
+              'User ID is null. Cannot update without a valid user ID.');
+        }
+        //await _unavailabilityRepository.deleteUnavailabilityEvent(userID, eventID);
+        _loadingState.add(RequestState.success(null));
+      } catch (e, stacktrace) {
+        logger.e(e, stackTrace: stacktrace);
+        _loadingState.add(RequestState.exception(e));
+      }
+    }();
   }
 
-  void updateSelectedYear(int year){
+  void editEventNavigate(eventID){
+    //navigate.add()
+  }
+
+  void updateSelectedMonth(int month) {
+    if (month < 1 && month > 12) {
+      return;
+    }
+    _selectedMonth.value = month;
+  }
+
+  void updateSelectedYear(int year) {
+    if(year < 0){
+      return;
+    }
+    _selectedYear.value = year;
   }
 
   @override
@@ -56,7 +119,6 @@ class CalendarViewModel extends FireAppViewModel
     _selectedMonth.close();
     _selectedYear.close();
     _navigate.close();
-    _submissionState.close();
-
+    _loadingState.close();
   }
 }
