b7de3c5ad35822f52836566d02a0d0a31b38e1fa
Steven (Quoc) <u7108792@anu.edu.au>
2024-05-01 15:07:43 +1000

    made eventId not null.

--- fireapp/lib/domain/models/unavailability/unavailability_time.dart
+++ fireapp/lib/domain/models/unavailability/unavailability_time.dart
@@ -1,20 +1,17 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-
 part 'unavailability_time.freezed.dart';
 part 'unavailability_time.g.dart';
-
 // Immutable data class representing a volunteer's 'Unavailability time.
 @freezed
 class UnavailabilityTime with _$UnavailabilityTime{
   const factory UnavailabilityTime({
-    required int? eventId, // Only NULL when creating new instance
+    required int eventId,
     required int userId,
     required String title,
     required int periodicity, //Number of days between repeating event
     required DateTime startTime,
     required DateTime endTime,
   }) = _UnavailabilityTime;
-
   // Factory constructor to convert 'UnavailabilityTime' JSON to object
   factory UnavailabilityTime.fromJson(Map<String, Object?> json) =>
       _$UnavailabilityTimeFromJson(json);
