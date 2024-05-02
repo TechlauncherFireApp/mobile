2059418bdf93976890aab0e9fbf10a834cd5d05e
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2024-05-02 11:41:35 +1000

    use the data to do the event cards

--- fireapp/lib/presentation/calendar/calendar_navigation.dart
+++ fireapp/lib/presentation/calendar/calendar_navigation.dart
@@ -1,14 +1,8 @@
 import 'package:fireapp/domain/models/unavailability/unavailability_time.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../../domain/models/unavailability/unavailability_time.dart';
-
 part 'calendar_navigation.freezed.dart';
 @freezed
 class CalendarNavigation with _$CalendarNavigation {
-<<<<<<< HEAD
   const factory CalendarNavigation.eventDetail(UnavailabilityTime event) = EditEvent;
-=======
-  const factory CalendarNavigation.eventDetail(int eventId) = EditEvent;
->>>>>>> Yuetian/Feature/calendar-view
 }
