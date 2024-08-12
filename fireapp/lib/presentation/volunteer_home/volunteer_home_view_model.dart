442e32e5ff03c29b485662ce82cab31d75824000
ethantr <ethantrossi@gmail.com>
2024-08-12 11:19:25 +1000

    Basic setup for volunteer_home.dart and volunteer_home_view_model.dart

---
+++ fireapp/lib/presentation/volunteer_home/volunteer_home_view_model.dart
@@ -0,0 +1,17 @@
+import 'package:injectable/injectable.dart';
+
+import '../fireapp_view_model.dart';
+
+@injectable
+class VolunteerHomeViewModel extends FireAppViewModel{
+
+
+
+
+  @override
+  Future<void> dispose() {
+    // TODO: implement dispose
+    throw UnimplementedError();
+  }
+
+}
