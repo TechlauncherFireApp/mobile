7fbdcbe31fd0ab394e9456bd16f440dd448adf85
juliusalberto <u7724204@anu.edu.au>
2024-08-19 09:54:19 +1000

    Added date time format constants

--- fireapp/lib/base/date_contants.dart
+++ fireapp/lib/base/date_contants.dart
@@ -1,5 +1,7 @@
 const String presentableDate = 'yyyy-MM-dd';
 const String calendarAppBarPresentableDate = 'MMM yyyy';
+const String homeDate = "EEEE d MMMM";
+const String homeTime = "h:mm a";
 final DateTime globalFirstSelectableDate = DateTime(2020);
 final DateTime globalLastSelectableDate = DateTime(2100);
 DateTime get globalInitialDate => DateTime.now();
