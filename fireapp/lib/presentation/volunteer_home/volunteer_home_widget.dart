f66ed9f2d9858b972aea8d5bb65dd65fd8b74000
juliusalberto <u7724204@anu.edu.au>
2024-08-19 09:55:28 +1000

    Fixed the issue with pull request

--- fireapp/lib/presentation/volunteer_home/volunteer_home_widget.dart
+++ fireapp/lib/presentation/volunteer_home/volunteer_home_widget.dart
@@ -1,4 +1,7 @@
+import 'package:fireapp/base/spaced_by.dart';
 import 'package:fireapp/presentation/volunteer_home/volunteer_home_view_model.dart';
+import 'package:fireapp/style/theme.dart';
+import 'package:fireapp/widgets/request_state_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
@@ -7,28 +10,19 @@ import '../../base/widget.dart';
 import '../../domain/models/shift.dart';
 import '../../domain/request_state.dart';
 import '../fireapp_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../style/typography.dart';
+import '../../style/colors.dart';
+import '../../base/date_contants.dart';
 
-class VolunteerHome extends StatelessWidget {
-  const VolunteerHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: HomeView(),
-      ),
-    );
-  }
-}
-
-class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+class VolunteerHomePage extends StatefulWidget {
+  const VolunteerHomePage({super.key});
 
   @override
-  State createState() => _HomeState();
+  State createState() => _VolunteerHomePageState();
 }
 
-class _HomeState extends FireAppState<HomeView>
+class _VolunteerHomePageState extends FireAppState<VolunteerHomePage>
     // with Navigable<CalendarNavigation, HomeView>
     implements
         ViewModelHolder<VolunteerHomeViewModel> {
@@ -44,83 +38,79 @@ class _HomeState extends FireAppState<HomeView>
 
   @override
   Widget build(BuildContext context) {
-      return StreamBuilder<RequestState<List<Shift>>>(stream: viewModel.shiftsStream,
-          builder: (context, snapshot) {
-            print('StreamBuilder received update: ${snapshot.data}');
-            if (!snapshot.hasData) {
-              print('No data in snapshot');
-              return const Center(child: CircularProgressIndicator());
+      return Scaffold(
+        body: SafeArea (
+          child: RequestStateWidget.stream<List<Shift>>
+          (state: viewModel.shiftsStream,
+          retry: () => viewModel.fetchShifts(),
+          child: (context, shifts) {
+            if (shifts.isEmpty) {
+              return Center(child: Text(AppLocalizations
+                  .of(context)
+                  ?.no_shift_avail ?? ""));
             }
 
-            final state = snapshot.data!;
+            final nextShift = shifts.first;
 
-            if (state is InitialRequestState) {
-              print('Initial state');
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is LoadingRequestState) {
-              print('Loading state');
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is ExceptionRequestState) {
-              var exceptionState = state as ExceptionRequestState;
-              print('Exception state: ${exceptionState.exception}');
-              return Center(child: Text('Error: ${exceptionState.exception}'));
-            } else if (state is SuccessRequestState<List<Shift>>) {
-              print('Success state with ${state.result.length} shifts');
-              final shifts = state.result;
-              if (shifts.isEmpty) {
-                return const Center(child: Text("No shifts available."));
-              }
+            return SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.all(1.5.rdp()),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              AppLocalizations
+                                  .of(context)
+                                  ?.next_shift ?? "",
+                              style: textTheme.titleLarge,
+                            ),
+                            _buildShiftCard(nextShift, isNextShift: true),
+                          ].spacedBy(1.0.rdp()),
+                        ),
 
-              final nextShift = shifts.first;
-
-              return SingleChildScrollView(
-                  child: Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Your Next Shift:',
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
-                          ),
-                          SizedBox(height: 16),
-                          _buildShiftCard(nextShift, isNextShift: true),
-                          const SizedBox(height: 24,),
-                          const Text(
-                            'Upcoming Shifts:',
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
-                          ),
-                          SizedBox(height: 16,),
-                          ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: shifts.length - 1,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                  padding: const EdgeInsets.only(bottom: 8.0),
-                                  child: _buildShiftCard(shifts[index + 1])
-                              );
-                            },
-                          )
-                        ],
-                      )
-                  )
-              );
-            } else {
-              return const Center(child: Text('Unknown state'));
-            }
-          });
-
+                        // Upcoming Shifts section
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              AppLocalizations
+                                  .of(context)
+                                  ?.upcoming_shift ?? "",
+                              style: textTheme.titleLarge,
+                            ),
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: shifts.length - 1,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: _buildShiftCard(shifts[index + 1])
+                                );
+                              },
+                            )
+                          ].spacedBy(1.0.rdp()),
+                        )
+                      ].spacedBy(1.5.rdp()),
+                    )
+                )
+            );
+          })
+        )
+      );
   }
 
   Widget _buildShiftCard(Shift shift, {bool isNextShift = false}) {
-    final dateFormat = DateFormat('EEEE d MMMM');
-    final timeFormat = DateFormat('h:mm a');
+    final dateFormat = DateFormat(homeDate);
+    final timeFormat = DateFormat(homeTime);
 
     return Card(
-      color: isNextShift ? Colors.red[200] : Colors.blue[200],
+      color: isNextShift ? homeRedColor : homeBlueColor,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(1.0.rdp()),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -128,14 +118,15 @@ class _HomeState extends FireAppState<HomeView>
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(dateFormat.format(shift.startTime),
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                Text('Shift ID: ${shift.shiftId}')
-              ],
+                      style: textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w700)),
+                Text(AppLocalizations.of(context)!.shift_id_display_home(shift.shiftId))
+              ].spacedBy(1.0.rdp()),
             ),
-            const SizedBox(height: 8),
-            const SizedBox(height: 8,),
-            Text('${timeFormat.format(shift.startTime)} - ${timeFormat.format(shift.endTime)}')
-          ]
+            Text(AppLocalizations.of(context)!.shift_time_range(
+                timeFormat.format(shift.startTime),
+                timeFormat.format(shift.endTime)
+            ))
+          ].spacedBy(1.0.rdp())
         ),
       ),
     );
