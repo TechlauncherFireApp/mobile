15de89acebb59bcc6a0d4d527a7218b8a395031d
Tai Ha <taihavn1101@gmail.com>
2024-09-28 19:57:52 +1000

    fix the wrong version of firebase_options.dart

--- fireapp/lib/firebase_options.dart
+++ fireapp/lib/firebase_options.dart
@@ -14,7 +14,7 @@ import 'package:flutter/foundation.dart'
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
 /// ```
-clDefaultFirebaseOptions {
+class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return web;
